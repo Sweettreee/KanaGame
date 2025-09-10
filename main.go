a5faefec282c70ea2d7d65ae01ec2edd771a4dad
kanai1 <ksh30918@kakao.com>
2025-09-10 21:44:09 +0900

    test: redisclient 연결 테스트

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"KanaGame/redisclient"
+	"fmt"
+)
+
+func main() {
+	redisclient.InitRedis("localhost:6379", "", 0)
+
+	err := redisclient.RDB.Set(redisclient.Ctx, "greeting", "Hello Go Redis", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := redisclient.RDB.Get(redisclient.Ctx, "greeting").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Redis에서 가져온 값:", val)
+}
