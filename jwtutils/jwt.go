c46fff24c90de186859b92f6c75eae78eab0df58
Sweettreee <xhrlehgus1234@naver.com>
2025-09-13 23:15:54 +0900

    jwt token func

---
+++ jwtutils/jwt.go
@@ -0,0 +1,46 @@
+package jwtutils
+
+import (
+	"fmt"
+	"time"
+	"github.com/golang-jwt/jwt/v5"
+) // v5 문서의 API와 예제 기준
+
+var secretKey = []byte("secret-key")
+
+func CreateToken(uid string) (string, error) { // 패키지 외부에서 사용하려면 식별자를 대문자로 시작해 export해야 함
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, 
+		// create JWT token by jew.NewWithClaims()
+		// method : HS256
+		jwt.MapClaims{ // relevant information
+		"UID" : uid,
+		"ISS" : "kanagame",
+		"exp" : time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	// os.Getenv("JWTSCERETKEY")
+	// fmt.Println(os.Getenv("JWTSCERETKEY"));
+
+	tokenString, err := token.SignedString(secretKey) // secretekey
+	if err != nil {
+		return "",err
+	}
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+   token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	// For parsing and verifying
+      return secretKey, nil
+   })
+  
+   if err != nil {
+      return err
+   }
+  
+   if !token.Valid {
+      return fmt.Errorf("invalid token")
+   }
+  
+   return nil
+}
